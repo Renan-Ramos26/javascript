284b991f55620dad8a8121b8c6aaadf37f8220f1
Renan Sousa Ramos <212819199+Renan-Ramos26@users.noreply.github.com>
2025-08-15 06:34:11 -0300

    comi

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+while (c <= 7) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+var a = 1
+do {
+    console.log(`passo ${a}`)
+    a++
+} while (a <= 6)
+
+for(var b = 1;b <= 6; b++) {
+    console.log(b)
+}
+
+/* console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')  */   
