dc10b4c76d429a835f9728965f78e5ae4d09e313
Renan Sousa Ramos <212819199+Renan-Ramos26@users.noreply.github.com>
2025-08-18 12:38:20 -0300

    comeco

--- aula15/ex015/modelo/script.js
+++ aula15/ex015/modelo/script.js
@@ -3,15 +3,22 @@ function analisar(n){
         window.alert('[ERRO] Digite um valor')
     } else {
         let txt = document.getElementById('txtn')
-        let res = document.getElementById('valt')
+        let res = document.getElementById('res')
         let n = Number(txt.value)
         if (n <= 0 || n >= 100){
             window.alert("[Erro] Valor fora dos parâmetros! Tente novamente!")
         } else {
-            res.innerHTML = `Valor ${n} adicionado.`
+            res.innerHTML += `Valor ${n} adicionado. <br>`
            // num.push(n)
            // let item = document.createElement('option')
-            
         }
     }
+    return n
+}
+var num = []
+num.push(n)
+
+function final() {
+    fim = document.getElementById('fim')
+    fim.innerHTML = `Ao todo, temos ${num[0]} números cadastrados. <br>`
 }
